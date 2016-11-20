57fc246aa23f50b258b03c33a6a0388aee5fd072
DongZhiChen <644495218@qq.com>
2016-11-20 12:37:28 +0800

    Initial Commit

---
+++ NeiHanDuanZi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NeiHanDuanZi
+//
+//  Created by 陈东芝 on 16/11/20.
+//  Copyright © 2016年 陈东芝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
